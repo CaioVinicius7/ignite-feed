03827842efaeda408650bcf97df95de361c42225
Caio Vinícius <caio1525pereira@gmail.com>
2022-06-02 15:31:56 -0300

    Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import Post from "./Post";
+import { Post } from "./Post";
 
 function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Caio Vinícius"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+      />
+      <Post
+        author="Ellen Bessa"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+      />
     </div>
   )
 }

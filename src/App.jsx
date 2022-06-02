3f4e1a4289d017d0cbb7ab83eaed7c3bb1a4bc35
Caio Vinícius <caio1525pereira@gmail.com>
2022-06-02 16:04:10 -0300

    CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import "./styles.css";
+import { Header } from "./components/header";
 import { Post } from "./Post";
 
 function App() {
   return (
     <div>
+      <Header></Header>
+
       <Post
         author="Caio Vinícius"
         content="Lorem ipsum dolor sit amet consectetur, adipisicing elit."
@@ -12,7 +16,7 @@ function App() {
         content="Lorem ipsum dolor sit amet consectetur, adipisicing elit."
       />
     </div>
-  )
+  );
 }
 
 export default App

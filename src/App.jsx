a9fd511f233cec576967ce00e6ea58328a50ecc5
Caio Vinícius <caio1525pereira@gmail.com>
2022-06-02 17:35:55 -0300

    Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 import { Header } from "./components/header";
 import { Post } from "./Post";
+
+import "./global.css";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
       <Header></Header>
 
-      <Post
-        author="Caio Vinícius"
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit."
-      />
-      <Post
-        author="Ellen Bessa"
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Caio Vinícius"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+          />
+          <Post
+            author="Ellen Bessa"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+          />
+        </main>
+      </div>
     </div>
   );
 }

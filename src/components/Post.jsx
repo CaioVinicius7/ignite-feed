4cfd288b9ef893d52309a6939011f62ce11ccefe
Caio Vinícius <caio1525pereira@gmail.com>
2022-06-06 17:56:46 -0300

    Inserindo comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,10 +8,11 @@ import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post({ author, publishedAt, content }) {
-  const [ comments, setComments ] = useState([
-    1,
-    2
+  const [comments, setComments] = useState([
+    "Post muito bacana, hein?!"
   ]);
+
+  const [ newCommentText, setNewCommentText ] = useState("");
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR
@@ -24,7 +25,13 @@ export function Post({ author, publishedAt, content }) {
 
   function handleCreateNewComment() {
     event.preventDefault();
-    setComments([...comments, comments.length + 1]);
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText("");
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
   }
 
   return (
@@ -61,7 +68,10 @@ export function Post({ author, publishedAt, content }) {
         <strong> Deixe aqui seu feedback </strong>
 
         <textarea 
+          name="comment"
           placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
         />
 
         <footer>
@@ -73,7 +83,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment />
+          return <Comment content={comment} />
         })}
       </div>
     </article>
